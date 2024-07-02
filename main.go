27d315eeb11e78db67b826d0982ed209701eabc3
iampushkar <m4manishpushkar@gmail.com>
2024-07-02 23:07:42 +0530

    add create payment api

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello")
+	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
+	http.HandleFunc("/health", handleHealth)
+
+	log.Println("Listening on localhost:4242")
+	var err error = http.ListenAndServe("localhost:4242", nil)
+	if err == nil {
+		log.Fatal(err)
+	}
+}
+
+func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("ENDPOINT CALLED!")
+}
+
+func handleHealth(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("OKK!")
 }
